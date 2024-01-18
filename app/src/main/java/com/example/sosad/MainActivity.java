d8a266014ae7467869eaf07446f67374ce8bc2a8
Pannawadeeg <108852251+Pannawadeeg@users.noreply.github.com>
2024-01-18 11:20:24 +0700

    first commit

--- app/src/main/java/com/example/sosad/MainActivity.java
+++ app/src/main/java/com/example/sosad/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.sosad;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
-
 public class MainActivity extends AppCompatActivity {
 
         public static int TIME_OUT = 3000;
-
-
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/sosad/MainActivity2.java
+++ app/src/main/java/com/example/sosad/MainActivity2.java
@@ -2,9 +2,7 @@ package com.example.sosad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -34,7 +32,6 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
@@ -43,13 +40,11 @@ import java.util.Arrays;
 
 public class MainActivity2 extends AppCompatActivity {
 
-
-    TextView result, confidence;
+    TextView result , confidence;
     ImageView imageView;
     Button picture, selectBtn;
     TextToSpeech textToSpeech; // ตัวแปร TextToSpeech
     int imageSize = 224;
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,9 +75,10 @@ public class MainActivity2 extends AppCompatActivity {
         });
 
         // ตรวจสอบการอนุญาตใช้งาน RECORD_AUDIO
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1); // ใช้ 1 หรือค่าคงที่อื่น ๆ ที่คุณต้องการ
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 2); // You can use a different request code if needed
         }
+
 
         textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
 
@@ -91,8 +87,6 @@ public class MainActivity2 extends AppCompatActivity {
                     int result = textToSpeech.setLanguage(Locale.US); // ตั้งค่าภาษา
                     if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                         Log.e(TAG, "Language not supported");
-                    } else {
-                        // TTS พร้อมใช้งาน ตอนนี้คุณสามารถเรียก speakText() ได้
                     }
                 } else {
                     Log.e(TAG, "TTS initialization failed");
@@ -109,6 +103,7 @@ public class MainActivity2 extends AppCompatActivity {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
     }
+
 
     // Open the camera
     private void openCamera() {
@@ -130,12 +125,10 @@ public class MainActivity2 extends AppCompatActivity {
         }
         super.onDestroy();
     }
-
-
+// ประมวผล
     public void classifyImage(Bitmap image) {
         try {
             Model model = Model.newInstance(getApplicationContext());
-
             // Create inputs for the model.
             TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
             ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
@@ -152,36 +145,20 @@ public class MainActivity2 extends AppCompatActivity {
                     byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
                 }
             }
-
             inputFeature0.loadBuffer(byteBuffer);
-
-            // Run model inference and get the result.
-
             Model.Outputs outputs = model.process(inputFeature0);
             TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
             model.close();
-
             float[] confidences = outputFeature0.getFloatArray();
-
             List<Integer> topClasses = findTopNClasses(outputFeature0.getFloatArray(), 3);
 
             String[] classes = {"Airedale", "American Staffordshire Terrier", "Appenzeller", "Basenji", "Beagle", "Bernese Mountain Dog", "Blenheim spaniel", "Bluetick", "Boston Bull", "Boxer",
-                    "Brabancon Griffon", "Bull Mastiff", "Chihuahua", "Chow", "Cocker Spaniel", "Collie", "Dingo", "Doberman", "English Foxhound", "EntleBucher",
-                    "Flat Coated Retriever", "French Bulldog", "German Shepherd", "Giant Schnauzer", "Golden Retriever", "Gordon Setter", "Great Dane", "Great Pyrenees", "Greater Swiss Mountain Dog", "Groenendael",
-                    "Ibizan Hound", "Irish Setter", "Irish Terrier", "Irish Water Spaniel", "Irish Wolfhound", "Italian Greyhound", "Japanese spaniel", "Keeshond", "Kelpie", "Kerry Blue Terrier",
-                    "Komondor", "Kuvasz", "Labrador Retriever", "Lakeland Terrier", "Leonberg", "Lhasa", "Malamute", "Malinois", "Maltese dog", "Mexican Hairless",
-                    "Miniature Pinscher", "Miniature Poodle", "Miniature Schnauzer", "Newfoundland", "Norfolk Terrier", "Norwegian Elkhound", "Norwich Terrier", "Pekinese", "Pomeranian", "Pug",
-                    "Redbone", "Rhodesian Ridgeback", "Rottweiler", "Saint Bernard", "Saluki", "Samoyed", "Shihtzu", "Siberian Husky", "Silky Terrier", "Staffordshire Bullterrier",
+                    "Brabancon Griffon", "Bull Mastiff", "Chihuahua", "Chow", "Cocker Spaniel", "Collie", "Dingo", "Doberman", "English Foxhound", "EntleBucher", "Flat Coated Retriever", "French Bulldog",
+                    "German Shepherd", "Giant Schnauzer", "Golden Retriever", "Gordon Setter", "Great Dane", "Great Pyrenees", "Greater Swiss Mountain Dog", "Groenendael",
+                    "Ibizan Hound", "Irish Setter", "Irish Terrier", "Irish Water Spaniel", "Irish Wolfhound", "Italian Greyhound", "Japanese spaniel", "Keeshond", "Kelpie", "Kerry Blue Terrier", "Komondor",
+                    "Kuvasz", "Labrador Retriever", "Lakeland Terrier", "Leonberg", "Lhasa", "Malamute", "Malinois", "Maltese dog", "Mexican Hairless", "Miniature Pinscher", "Miniature Poodle", "Miniature Schnauzer",
+                    "Newfoundland", "Norfolk Terrier", "Norwegian Elkhound", "Norwich Terrier", "Pekinese", "Pomeranian", "Pug", "Redbone", "Rhodesian Ridgeback", "Rottweiler", "Saint Bernard", "Saluki", "Samoyed", "Shihtzu", "Siberian Husky", "Silky Terrier", "Staffordshire Bullterrier",
                     "Standard Poodle", "Standard Schnauzer", "Toy Poodle", "Vizsla", "Walker Hound", "Weimaraner", "West Highland White Terrier", "Wire Haired Fox Terrier", "Yorkshire Terrier"};
-
-
-            // Sort topClasses in descending order of confidence scores
-            topClasses.sort(new Comparator<Integer>() {
-                public
-                int compare(Integer classIndex1, Integer classIndex2) {
-                    return Float.compare(confidences[classIndex2], confidences[classIndex1]);
-                }
-            });
 
             // Create an array to store the top 3 classes and their confidence scores
             String[] top3Classes = new String[Math.min(3, topClasses.size())];
@@ -193,32 +170,16 @@ public class MainActivity2 extends AppCompatActivity {
                 float confidenceScore = confidences[classIndex];
 
                 // Add a condition to check for valid confidence scores
-                if (!Float.isNaN(confidenceScore)) {
+                if (!Float.isNaN(confidenceScore) && confidenceScore >= 0) {
                     top3Classes[i] = className + ": " + String.format("%.1f%%", confidenceScore * 100);
                 }
             }
-
-            // Sort top3Classes array in descending order of confidence scores
-            Arrays.sort(top3Classes, new Comparator<String>() {
-                @Override
-                public int compare(String s1, String s2) {
-                    float confidence1 = Float.parseFloat(s1.split(": ")[1].replace("%", ""));
-                    float confidence2 = Float.parseFloat(s2.split(": ")[1].replace("%", ""));
-                    return Float.compare(confidence2, confidence1);
-                }
-            });
-
-// นำข้อมูลคลาสที่มีความมั่นใจสูงสุดมาแสดงใน TextView confidence
+            // นำข้อมูลคลาสที่มีความมั่นใจสูงสุดมาแสดงใน TextView confidence
             String topConfidenceClass = top3Classes[0]; // คลาสที่มีความมั่นใจสูงสุด
 
-// ตั้งค่าให้ TextView confidence แสดงคลาสที่มีความมั่นใจสูงสุด
-            confidence.setText(topConfidenceClass);
-
-
-
+            // แสดงผลลัพธ์
+           confidence.setText(topConfidenceClass);
             result.setText(classes[topClasses.get(0)]); // Display the top class in TextView result
-            confidence.setText(TextUtils.join("\n", top3Classes)); // Display the top 3 classes in TextView confidence
-
             speakText(result.getText().toString());
         } catch (IOException e) {
             // Handle the exception
@@ -248,24 +209,21 @@ public class MainActivity2 extends AppCompatActivity {
         return topClasses;
     }
 
-
     // Class to store class index and confidence score
-    private static
-    class ClassPrediction {
+    private static class ClassPrediction {
         private int classIndex;
         private float confidence;
 
-        public
-        ClassPrediction(int classIndex, float confidence) {
+        public ClassPrediction(int classIndex, float confidence) {
             this.classIndex = classIndex;
             this.confidence = confidence;
         }
-        public
-        int getClassIndex() {
+
+        public int getClassIndex() {
             return classIndex;
         }
-        public
-        float getConfidence() {
+
+        public float getConfidence() {
             return confidence;
         }
     }
@@ -310,6 +268,4 @@ public class MainActivity2 extends AppCompatActivity {
             classifyImage(imageBitmap);
         }
     }
-
 }
-
